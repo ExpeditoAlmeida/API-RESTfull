c61ef271bd8ca93c2d460fd26ba1e72ebfcbc756
ExpeditoAlmeida <expedito_dealmeida@outlook.com.br>
2020-07-11 01:23:27 -0300

    impl: serviço crud.

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +22,7 @@ import curso.api.rest.repositoy.UsuarioRepository;
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
-	@Autowired /* de fosse CDI seria @Inject */
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	/* Serviço RESTful */
@@ -30,12 +34,36 @@ public class IndexController {
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
 
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable(value = "id") Long id) {
+
+		usuarioRepository.deleteById(id);
+
+		return "ok";
+	}
+
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity<List<Usuario>> usuario() {
 
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+
+		Usuario Atualizarusuario = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(Atualizarusuario, HttpStatus.OK);
 	}
 
 }
