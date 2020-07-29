340957a8393d91f8c40ea2fea738212f76fd19ab
ExpeditoAlmeida <expedito_dealmeida@outlook.com.br>
2020-07-29 19:42:17 -0300

    config : pool de conexão hikari. Impl de cache para performance

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,7 @@ public class IndexController {
 
 	/* Serviço RESTful */
 	@GetMapping(value = "/{id}", produces = "application/json")
+	@Cacheable("cacheuser")
 	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
 
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
@@ -46,9 +48,12 @@ public class IndexController {
 	}
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> usuario() {
+	@Cacheable("cacheusuarios")
+	public ResponseEntity<List<Usuario>> usuario() throws InterruptedException {
 
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+
+		Thread.sleep(6000);
 
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
